4550c9410535e031ae447ee11fd2020d58e9c8f2
vannipluto <vannipluto@gmail.com>
2018-08-31 18:44:20 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -7,6 +7,8 @@ if(!$update)
   exit;
 }
 
+define("BOT_TOKEN", "659085083:AAHVwdPVn4E052VTv9z4yVFb7aLEZi5PPas");
+
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
@@ -19,7 +21,29 @@ $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
 $text = strtolower($text);
 
+$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/forwardMessage";
+
+// change image name and path
+// $postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("image.png")), 'caption' => $text);
+
+// chat_id: Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+// from_chat_id: Unique identifier for the chat where the original message was sent (or channel username in the format @channelusername)
+// message_id: Message identifier in the chat specified in from_chat_id
+
+$postFieldsForFW = array('chat_id' => '@fmonaca', 'from_chat_id' => $chatId, 'message_id' => $messageId);
+
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
+curl_setopt($ch, CURLOPT_URL, $botUrl); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postFieldsForFW);
+
+// read curl response
+$output = curl_exec($ch);
+
+
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
+$parameters = array('chat_id' => $chatId, "text" => $output);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
+?>
