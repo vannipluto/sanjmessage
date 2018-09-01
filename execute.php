fb8804a9370cae48d49bd0b3632225d16942bd68
vannipluto <vannipluto@gmail.com>
2018-09-01 02:57:21 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -42,7 +42,7 @@ $botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/forwardMessage";
 // from_chat_id: Unique identifier for the chat where the original message was sent (or channel username in the format @channelusername)
 // message_id: Message identifier in the chat specified in from_chat_id
 
-$postFieldsForFW = array('chat_id' => '@snjmsg2288', 'from_chat_id' => $chatId, 'message_id' => $messageId);
+$postFieldsForFW = array('chat_id' => '@SnjKky52288', 'from_chat_id' => $chatId, 'message_id' => $messageId);
 
 $ch = curl_init(); 
 curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
